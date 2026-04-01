135d3924614cd5fe32588e600d3d9679ec8c5e44
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-01 20:10:00 +0500

    pus

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Login from "./components/Auth/Login"
+// import Login from "./components/Auth/Login"
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
 import './App.css'
 
 function App() {
  
   return (
     <>
-    <Login/>
+    {/* <Login/> */}
+    rest karo 
+    <EmployeeDashboard/>
     </>
   )
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,19 @@
-
 import { useState } from "react";
 
-
 const Login = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
 
-  const [email , setemail] = useState('')
-  const [password , setpassword] = useState('')
 
   const submitHandler = (e) => {
     e.preventDefault();
-     return console.log("papan")
-  }
+     console.log(email, password); 
+
+
+    setemail("")
+    setpassword("")
+  };
   return (
     <>
       <div className="flex h-screen w-screen items-center justify-center p-25">
@@ -19,22 +21,31 @@ const Login = () => {
           <h1>Login</h1>
 
           <form onSubmit={submitHandler} className="flex flex-col items-center">
-
-            <input value={email} onChange={(e) => {
-              setemail(e.target.value)
-            }} required
+            <input
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+              required
               type="email"
               placeholder="enter your email"
               className="border-2 border-emerald-600 rounded-full p-3 m-3 text-xl outline-none bg-transparent placeholder:text-gray-100"
             />
 
-            <input  required
+            <input
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+              required
               type="password"
               placeholder="enter your password"
               className="border-2 border-emerald-600 rounded-full p-3 m-3 text-xl  outline-none bg-transparent placeholder:text-gray-300"
             />
 
-            <button className=" bg-emerald-600 rounded-full p-3 m-3 text-xl text-gray-300 outline-none  placeholder:text-white">Submit</button>
+            <button className=" bg-emerald-600 rounded-full p-3 m-3 text-xl text-gray-300 outline-none  placeholder:text-white">
+              Submit
+            </button>
           </form>
         </div>
       </div>
