a7797c8d6451aae1dca0550835c4bf83557f9ab7
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-01 19:38:58 +0500

    code

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+
+import { useState } from "react";
+
 
 const Login = () => {
+
+
+  const [email , setemail] = useState('')
+  const [password , setpassword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+     return console.log("papan")
+  }
   return (
     <>
       <div className="flex h-screen w-screen items-center justify-center p-25">
         <div className="border-2 border-emerald-600 p-4">
           <h1>Login</h1>
 
-          <form className="flex flex-col items-center">
-            <input required
+          <form onSubmit={submitHandler} className="flex flex-col items-center">
+
+            <input value={email} onChange={(e) => {
+              setemail(e.target.value)
+            }} required
               type="email"
               placeholder="enter your email"
               className="border-2 border-emerald-600 rounded-full p-3 m-3 text-xl outline-none bg-transparent placeholder:text-gray-100"
             />
 
             <input  required
-              type="text"
+              type="password"
               placeholder="enter your password"
               className="border-2 border-emerald-600 rounded-full p-3 m-3 text-xl  outline-none bg-transparent placeholder:text-gray-300"
             />
