a810ff7d583f1f52f64b467762dd744507033811
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-02 18:52:36 +0500

    new

--- src/Tasklist/TaskList.jsx
+++ src/Tasklist/TaskList.jsx
@@ -3,7 +3,39 @@ import React from 'react'
 const TaskList = () => {
   return (
     <>
-    
+        <div id='tasklist' className='flex justify-start gap-5 flex-nowrap overflow-x-auto h-[55%] bg-red-900 py-5  w-full mt-10'>
+
+
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+       <div className='bg-yellow-400 shrink-0 h-full w-75 rounded-xl'>
+
+       </div>
+
+
+       </div>
+
+
+  
     </>
   )
 }
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import Headaer from "../../Other/Headaer"
-import Tasklistnumber from "./Other/Tasklistnumber"
+import Headaer from "../../Other/Headaer";
+import Tasklistnumber from "../../Other/Tasklistnumber";
+import Tasklist from "../../Tasklist/TaskList";
+
 
 
 const EmployeeDashboard = () => {
   return (
     <>
-      <div className="p-10 bg-[#1c1c1c] ">
+      <div className="p-10 bg-[#1c1c1c] h-screen ">
         {/* EmployeeDashboard */}
         <Headaer />
         <Tasklistnumber/>
+        <Tasklist/>
       </div>
     </>
   );
