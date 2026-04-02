2a7b25540fad10e8cdd271dc413a8a34b15abeb3
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-02 13:21:44 +0500

    emscode

--- src/Other/Headaer.jsx
+++ src/Other/Headaer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Headaer = () => {
   return (
     <>
-    <div className='flex items-center justify-end'>
+    <div className='flex items-end justify-between'>
     <h1>hello mehboob</h1>
     <button>logout</button>
     </div>
