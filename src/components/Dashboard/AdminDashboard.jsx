9bb9885bd991b4050e20dc9220cfa17959750bbd
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-03 13:56:22 +0500

    save

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import Headaer from '../../Other/Headaer'
+import Headaer from "../../Other/Headaer";
 
 const AdminDashboard = () => {
   return (
-    <>
-    <div className='h-screen w-full p-10 '>
-     <Headaer/>
+    <div className="h-screen w-full p-10">
+      <Headaer />
 
-     <div className='bg-amber-800'>
-      <form>
-      
-       <h3>Task Title</h3>
-       <input type="text" placeholder='enter task' />
+      <form className="flex justify-between items-start">
 
+        <div className="w-1/2">
 
-       <h3>Description</h3>
-       <input type="text" placeholder='Description' />
-      
-      <textarea placeholder='text area' name="" id="" cols={30} rows={10}></textarea>
+          <div>
+            <h3>Task Title</h3>
+            <input type="text" placeholder="enter task" />
+          </div>
 
+          <div>
+            <h3>Date</h3>
+            <input type="date" />
+          </div>
 
-      <h3>Date</h3>
-      <input type="date"/>
-      <input type="text"/>
+          <div>
+            <h3>Assign</h3>
+            <input type="text" placeholder="Enter your employee name" />
+          </div>
+
+          <div>
+            <h3>Category</h3>
+            <input type="text" placeholder="design dev etc" />
+          </div>
+
+        </div>
+
+        <div className="w-1/2">
+          <h3>Description</h3>
+          <textarea
+            className="bg-amber-100 w-full"
+            placeholder="text area"
+            rows={10}
+          ></textarea>
+        </div>
 
       </form>
-     </div>
 
+      <button className="mt-5 px-4 py-2 bg-blue-500 text-white rounded">
+        Create Task
+      </button>
     </div>
-    </>
-    )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
