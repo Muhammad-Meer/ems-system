2fcefd138b0ff788137106490990be0e7d1ee015
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-03 15:53:20 +0500

    newcode

---
+++ src/Other/ALLtask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ALLtask = () => {
+  return (
+    <>
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded '>
+    <div className='bg-red-400 py-2'>
+
+     <h2>Mehboob</h2>
+     <h3>Make Ui Design</h3>
+     <h5>status</h5>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default ALLtask
--- src/Other/Headaer.jsx
+++ src/Other/Headaer.jsx
@@ -11,9 +11,7 @@ const Headaer = () => {
      <span className='text-3xl font-semibold'>Mehboob👋</span> </h1>
 
     <button className='bg-red-600 text-white text-lg font-medium px-5 py-2 rounded-small'>Log Out</button>
-
-  
-    </div>
+</div>
     </>
   )
 }
