a8f7997b6c799addcb92d2abf87cc5ad4190bca8
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-03 19:05:42 +0500

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import Login from "./components/Auth/Login"
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import './App.css'
+import { useEffect } from "react";
+import { setLocalfunc } from "./utils/Local.storage";
 
 function App() {
  
+     useEffect(() => {
+      setLocalfunc()
+     },)
+  
   return (
     <>
     <Login/>
--- src/utils/Local.storage.jsx
+++ src/utils/Local.storage.jsx
@@ -1,42 +1,5 @@
- const employee = [
-  {
-    "id": 1,
-    "email": "employee1@example.com",
-    "password": "123"
-  },
-  {
-    "id": 2,
-    "email": "employee2@example.com",
-    "password": "123"
-  },
-  {
-    "id": 3,
-    "email": "employee3@example.com",
-    "password": "123"
-  },
-  {
-    "id": 4,
-    "email": "employee4@example.com",
-    "password": "123"
-  },
-  {
-    "id": 5,
-    "email": "employee5@example.com",
-    "password": "123"
-  }
-]
 
-
-
-const admin = [
-  {
-    "id": 1,
-    "email": "admin1@example.com",
-    "password": "123"
-  }, 
-]
-
-[
+ const employees = [
   {
     "id": 1,
     "email": "employee1@example.com",
@@ -204,3 +167,17 @@ const admin = [
   }
 ]
 
+const admin = [{
+
+    "id": 1,
+    "email": "admin1@example.com",
+    "password": "123"
+  }]
+
+export   const  setLocalfunc = () => {
+    localStorage.setItem("employees", JSON.stringify(employees))
+}
+
+export   const  getLocalfunc = () => {
+
+}
