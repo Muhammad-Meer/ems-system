943b740a76e8c1e768c71568d443ada95e81e9e4
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-04 15:19:28 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useEffect } from "react";
 // import { setLocalfunc } from "./utils/Local.storage";
 // import { getLocalfunc } from "./utils/Local.storage";
+import { useState } from 'react'
 import './App.css'
 // files
 import Login from "./components/Auth/Login"
@@ -8,16 +9,34 @@ import Login from "./components/Auth/Login"
 // import AdminDashboard from "./components/Dashboard/AdminDashboard";
 
 
+
+
 function App() {
- 
-    //  useEffect(() => {
+     //  useEffect(() => {
     //   setLocalfunc()
     //   getLocalfunc()
     //  }, [])
+
+       const [user , setuser] = useState(null)
+
+       const handelLogin = (email  , password) => {
+
+        if(email == "admin@gmail.com" && password == "123") {
+          console.log("this is admin")
+        }
+        else if(email == "user@gmail.com" && password == "123" ) {
+          console.log("this is user")
+        }
+        else{
+          console.log("invalid Credientionals")
+        }
+       }
+
+       handelLogin("user@gmail.com", "123")
   
   return (
     <>
-    <Login/>
+    {!user ? <Login handelLogin={handelLogin}/>: "" }
     {/* <EmployeeDashboard/>
     <AdminDashboard/> */}
 
