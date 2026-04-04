3d572b7e4361da4cb673fa8302adce57672fd067
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-04 16:59:39 +0500

    newcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+
 // files
 import Login from "./components/Auth/Login"
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
 
 function App() {
 
-  const [user , setuser] = useState(null)
+  const [user, setuser] = useState(null)
 
   const handelLogin = (email, password) => {
 
-    if(email === "admin@gmail.com" && password === "123") {
+    if (email === "admin@gmail.com" && password === "123") {
       console.log("this is admin")
       setuser("admin")
     }
-    else if(email === "user@gmail.com" && password === "123") {
+    else if (email === "user@gmail.com" && password === "123") {
       console.log("this is user")
       setuser("user")
     }
-    else{
-      console.log("invalid Credentials")
+    else {
+      alert("invalid Credentials")
     }
   }
 
   return (
     <>
-      {!user ? <Login handelLogin={handelLogin}/> : "" }
+      {!user && <Login handelLogin={handelLogin} />}
+
+      {user === "admin" && <AdminDashboard />}
+      {user === "user" && <EmployeeDashboard />}
     </>
   )
 }
